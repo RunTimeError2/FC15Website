21a77da75913a06d443c87d6a793b46975f8d0f5
RunTimeError2 <songjiahao980530@126.com>
2017-10-09 19:28:36 +0800

    Added compile switches for windows/VS and ubuntu/g++

--- FC15/oj.py
+++ FC15/oj.py
@@ -3,6 +3,10 @@ import threading
 from FC15.models import FileInfo
 
 IS_RUNNING = 0
+COMPILE_MODE = 'WIN_VS'
+# COMPILE_MODE = 'LINUX_G++'
+FILE_SUFFIX = 'exe'
+# FILE_SUFFIX = 'dll'
 
 
 # Start running
@@ -16,8 +20,12 @@ def run():
 
 # Copy file
 def copy_file(username, file_name):
+    global COMPILE_MODE
     source_dir = 'fileupload/{0}/{1}'.format(username, file_name)
-    destin_dir = 'cpp_proj/cpp_proj/main.cpp'
+    if COMPILE_MODE == 'WIN_VS':
+        destin_dir = 'cpp_proj/cpp_proj/main.cpp'
+    if COMPILE_MODE == 'LINUX_G++':
+        destin_dir = 'linux_compile/main.cpp'
     if os.path.isfile(source_dir):
         open(destin_dir, 'wb').write(open(source_dir, 'rb').read())
         return True
@@ -27,8 +35,13 @@ def copy_file(username, file_name):
 
 # Copy exe file
 def copy_exe(username, file_name):
-    source_dir = 'cpp_proj/Debug/cpp_proj.exe'
-    destin_dir = 'fileupload/{0}/{1}.exe'.format(username, file_name[:-4])
+    global FILE_SUFFIX
+    global COMPILE_MODE
+    if COMPILE_MODE == 'WIN_VS':
+        source_dir = 'cpp_proj/Debug/cpp_proj.' + FILE_SUFFIX
+    if COMPILE_MODE == 'LINUX_G++':
+        source_dir = 'linux_compile/main.' + FILE_SUFFIX
+    destin_dir = 'fileupload/{0}/{1}.{2}'.format(username, file_name[:-4], FILE_SUFFIX)
     if os.path.isfile(source_dir):
         open(destin_dir, 'wb').write(open(source_dir, 'rb').read())
         return True
@@ -53,7 +66,11 @@ class compile_thread(threading.Thread):
                     copy_result = copy_file(file.username, file.exact_name)
                     if copy_result:
                         # use visual studio to compile the project
-                        compile_result = os.system('devenv cpp_proj/cpp_proj.sln /rebuild > result.txt')
+                        global COMPILE_MODE
+                        if COMPILE_MODE == 'WIN_VS':
+                            compile_result = os.system('devenv cpp_proj/cpp_proj.sln /rebuild > result.txt')
+                        if COMPILE_MODE == 'LINUX_G++':
+                            compile_result = os.system('g++ -o main.' + FILE_SUFFIX + ' main.cpp')
                         file.is_compiled = 'Compiled'
                     if compile_result == 0:
                         file.is_compile_success = 'Successfully compiled'
